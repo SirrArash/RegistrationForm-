bf84191c6bbea949599ae6df869c6e70df99265a
Arash Ebrahimzadeh <arash.ebrahimzadeh88@gmail.com>
2023-06-23 23:44:10 +0330

    first-commit

---
+++ form/src/components/Login.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import { validate } from "./validate";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { notify } from "./toast";
+import styles from "./SignUp.module.css";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isAccepted: false,
+  });
+
+  const [errors, setErrors] = useState({});
+  const [touched, setTouched] = useState({});
+
+  useEffect(() => {
+    setErrors(validate(data, "login"));
+    // console.log(errors);
+  }, [data, touched]);
+
+  const changeHandler = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+
+    // console.log(data);
+  };
+
+  const focusHandler = (event) => {
+    setTouched({ ...touched, [event.target.name]: true });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (!Object.keys(errors).length) {
+      notify("You loged in successfully", "success");
+    } else {
+      notify("Invalid data", "error");
+      setTouched({
+        name: true,
+        email: true,
+        password: true,
+        confirmPassword: true,
+        isAccepted: true,
+      });
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={submitHandler} className={styles.formContainer}>
+        <h2 className={styles.header}>Login</h2>
+
+        <div className={styles.formField}>
+          <label>Email</label>
+          <input
+            className={
+              errors.email && touched.email
+                ? styles.uncompleted
+                : styles.formInput
+            }
+            type="email"
+            name="email"
+            value={data.email}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+          {errors.email && touched.email && <span>{errors.email}</span>}
+        </div>
+        <div className={styles.formField}>
+          <label>Password</label>
+          <input
+            className={
+              errors.password && touched.password
+                ? styles.uncompleted
+                : styles.formInput
+            }
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+          {errors.password && touched.password && (
+            <span>{errors.password}</span>
+          )}
+        </div>
+
+        <div className={styles.formButtons}>
+          <Link to="/signup">Sign Up</Link>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Login;
